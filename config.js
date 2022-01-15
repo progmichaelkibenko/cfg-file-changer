fc39316f92a39218fb6717c4967c96abad2493d3
Progmichaelkibenko <Progmichaelkibenko@gmail.com>
2022-01-15 23:06:37 +0200

    add logs

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+  folderPath: "./Public",
+  ignoreFolders: [],
+  model: {
+    filePath: "ICgraph.cfg",
+    findText: "רכיבה על י אסורה",
+    replaceTo: "רכיבה על יהמהלכגהןגכהמגדכחךהמדגחהמחלדגכמהדגמהמדגכמהדגמכהמדגכמה אסורה",
+    resultFilePath: "ICgraph.cfg",
+    prevFilePath: "ICgraph_old3.cfg",
+  },
+};
+
+module.exports = { config };
--- index.js
+++ index.js
@@ -1,19 +1,78 @@
 const fs = require("fs");
-var Iconv = require('iconv').Iconv;
+const Iconv = require("iconv").Iconv;
 
+const { config } = require("./config");
 
-const FILE_PATH = process.env.FILE_PATH || "BellPark/ICgraph.cfg";
+function decode({ fileBuffer, from, to }) {
+  var iconv = new Iconv(from, to);
+  var buffer = iconv.convert(fileBuffer);
+  return buffer.toString("utf8");
+}
 
-const ENCODING = process.env.FILE_PATH || 'utf-8';
+function encode({ content, from, to }) {
+  const originalBuffer = Buffer.from(content, "utf-8");
+  const iconv = new Iconv(from, to);
+  const buffer = iconv.convert(originalBuffer);
+  return buffer;
+}
 
-function decode(content) {
-    var iconv = new Iconv('CP1255', 'UTF-8//TRANSLIT//IGNORE');
-    var buffer = iconv.convert(content);
-    return buffer.toString('utf8');
-  };
+const writeFile = ({ filePath, content }) => {
+  fs.writeFileSync(filePath, content);
+};
 
-fs.readFile(FILE_PATH, (err, data) => {
-    const formattedData = decode(data);
-    fs.writeFileSync('BellPark/ICgraph2.cfg', formattedData)
-  
-});
+const renameFile = ({ oldPath, newPath }) => {
+  fs.renameSync(oldPath, newPath);
+};
+
+const rewriteFile = ({ folderPath, model }) => {
+  const { filePath, findText, replaceTo, resultFilePath, prevFilePath } = model;
+  try {
+    const originalFilePath = `${folderPath}/${filePath}`;
+    const fileBuffer = fs.readFileSync(originalFilePath);
+    const originalFormattedData = decode({
+      fileBuffer,
+      from: "CP1255",
+      to: "UTF-8//TRANSLIT//IGNORE",
+    });
+    const newFormattedData = originalFormattedData.replace(findText, replaceTo);
+
+    if (resultFilePath === prevFilePath) {
+      throw new Error("The result file path cant be equal to prevFilePath");
+    }
+
+    // save the old file
+    const oldFileWritablePath = `${folderPath}/${prevFilePath}`;
+    renameFile({ oldPath: originalFilePath, newPath: oldFileWritablePath });
+
+    // save the new file
+    const newFileWritablePath = `${folderPath}/${resultFilePath}`;
+    writeFile({
+      filePath: newFileWritablePath,
+      content: encode({
+        content: newFormattedData,
+        from: "UTF-8//TRANSLIT//IGNORE",
+        to: "CP1255",
+      }),
+    });
+    console.log(`${filePath} Done =))`);
+  } catch (err) {
+    console.log(`Failed to make rewrite of ${filePath} =((`);
+  }
+};
+
+const isFolder = (path) => fs.statSync(path).isDirectory();
+
+const start = () => {
+  const { folderPath, ignoreFolders, model } = config;
+
+  const files = fs.readdirSync(folderPath);
+
+  for (file of files) {
+    const path = `${folderPath}/${file}`;
+    if (isFolder(path)) {
+      rewriteFile({ folderPath: path, model });
+    }
+  }
+};
+
+start();
